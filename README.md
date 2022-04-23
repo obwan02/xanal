7309d6364ffe17f0203d582ef55826a0feed1f8d
obwan02 (WSL) <obwan02@hotmail.co.nz>
2022-04-23 19:02:20 +1200

    Updated Cargo.toml metadata and improved module structure

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # XANAL
-The xor analyser written in rust. In early beta
+The Xor ANALyser written in rust. This is a small project, mainly intended to be used in simple CTF challenges.
 
-## TODOs
+## TODOs (In no particular order)
+- [ ] Move test files to test folder (and make proper tests)
+- [x] Update Cargo.toml to have correct info, and make sure it is displayed in clap 
 - [ ] Maximum key size should be chosen algorithmiclly (i.e. for small input sizes (10-20 chars) max key input should be between 0 and 10) or maybe just a table for small values.
 - [ ] Better logging
 - [ ] Although crib and search is implemented in `clap` and in rust, need to join them
-- [ ] Make proper README.md
-- [ ] Actually make github repo
+- [x] Make proper README.md
+- [x] Actually make github repo
 - [ ] Loading bar for really long files?
-- [ ] Move key guessing function outside lib.rs
+- [x] Move key guessing functions outside lib.rs
 - [ ] Improve most common byte evaluator
 - [ ] Add posibility for multiple different keys to be found
 - [ ] Add likelihood of the key found being correct
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use clap::{ArgGroup, Parser};
 use flexi_logger::LoggerHandle;
-use log::{debug, error, info, warn};
-use simple_error::{simple_error, SimpleError};
+use log::{error, info, warn};
 use std::{
     fs::{File, OpenOptions},
     io::{stdin, Read, Write},
@@ -9,9 +8,10 @@ use std::{
 };
 
 mod kl_anal;
+mod key_guess;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author = "Oliver W. (obwan02)", version, about = "An Xor ANALyser for cryptanalysis", long_about = None)]
 #[clap(group(
             ArgGroup::new("method")
                 .required(true)
@@ -118,124 +118,7 @@ pub struct Config {
     pub key_length_only: bool,
 }
 
-pub enum GuessMethod<'a, 'b> {
-    MostCommon(u8),
-    Crib(&'a [u8], usize),
-
-    // First argument is the crib
-    // second argument is search
-    CribAndSearch(&'a [u8], &'b [u8]),
-}
-
-impl<'a, 'b> GuessMethod<'a, 'b> {
-    // Checks if the guessing method is valid
-    // for a certain key length
-    fn is_valid(&self, data: &[u8], key_length: usize) -> Result<(), SimpleError> {
-        use GuessMethod::*;
-        match &self {
-            MostCommon(_) => Ok(()),
-            CribAndSearch(crib, _) if key_length > crib.len() => {
-                Err(simple_error!("The crib is shorter than the key length"))
-            }
-            Crib(crib, _) if key_length > crib.len() => {
-                Err(simple_error!("The crib is shorter than the key length"))
-            }
-            Crib(crib, offset) if offset + crib.len() > data.len() => {
-                Err(simple_error!("The crib is offset too far into the file"))
-            }
-            _ => Ok(()),
-        }
-    }
-
-    fn get_key(&self, data: &[u8], key_length: usize) -> Result<Vec<u8>, SimpleError> {
-        use GuessMethod::*;
-
-        if let Err(e) = self.is_valid(data, key_length) {
-            return Err(e);
-        }
-
-        match self {
-            CribAndSearch(crib, search) => {
-                let limit = data.len() - crib.len();
-                for offset in 0..limit {
-                    let key_guess: Vec<u8> = data
-                        .iter()
-                        .skip(offset)
-                        .take(key_length)
-                        .enumerate()
-                        .map(|(i, &x)| x ^ crib[i])
-                        .collect();
-
-                    let data_test = decrypt(data, &key_guess);
-                    let mut success = false;
-                    let mut si = 0;
-                    for (i, x) in data_test.enumerate() {
-                        let val = x ^ key_guess[i % key_guess.len()];
-                        if val == search[si] {
-                            si += 1;
-                        } else {
-                            si = 0;
-                        }
-
-                        if si == search.len() - 1 {
-                            success = true;
-                        }
-                    }
-
-                    if success {
-                        return Ok(key_guess);
-                    }
-                }
-
-                Err(simple_error!("Could not find a suitable key"))
-            }
-            _ => {
-                let mut key = Vec::with_capacity(key_length);
-                for i in 0..key_length {
-                    key.push(self.get_key_at(data, i, key_length));
-                }
-
-                Ok(key)
-            }
-        }
-    }
-
-    fn get_key_at(&self, data: &[u8], key_index: usize, key_length: usize) -> u8 {
-        use GuessMethod::*;
-        match &self {
-            MostCommon(common) => {
-                let mut freqs = [0usize; 256];
-                data.iter()
-                    .skip(key_index)
-                    .step_by(key_length)
-                    .for_each(|x| freqs[*x as usize] += 1);
-                let most_freq = freqs
-                    .iter()
-                    .enumerate()
-                    .reduce(|(ci, cx), (i, x)| if x > cx { (i, x) } else { (ci, cx) })
-                    .unwrap()
-                    .0 as u8;
-
-                debug!("Most frequent byte found was {:#x}", most_freq);
-                most_freq ^ common
-            }
-            Crib(crib, offset) => data[offset + key_index] ^ crib[key_index],
-            CribAndSearch(_, _) => {
-                unimplemented!("get_key_at cannot be used for enum variant CribAndSearch")
-            }
-        }
-    }
-}
-
-pub fn guess_key(
-    data: &[u8],
-    key_length: usize,
-    method: GuessMethod,
-) -> Result<Vec<u8>, SimpleError> {
-    method.get_key(data, key_length)
-}
-
-fn decrypt<'a>(data: &'a [u8], key: &'a [u8]) -> impl Iterator<Item = u8> + 'a {
+pub fn decrypt<'a>(data: &'a [u8], key: &'a [u8]) -> impl Iterator<Item = u8> + 'a {
     data.iter()
         .enumerate()
         .map(|(i, &x)| x ^ key[i % key.len()])
@@ -270,6 +153,8 @@ fn read_input(config: &Config) -> Vec<u8> {
 }
 
 pub fn run(config: Config, mut logger: LoggerHandle) {
+    use key_guess::*;
+
     if config.verbose {
         logger.parse_new_spec("debug").unwrap();
     }
